8683c697a5081c2f31a56e0ea4318ee8f5811ab5
SanjayDDKK <sanjaydurai.kannadevan@techavidity.com>
2025-08-04 03:40:28 -0400

    Delete Neo4jTestAgent/genai-integration-langchain directory

--- Neo4jTestAgent/genai-integration-langchain/bot.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-from agent import generate_response
-
-st.title("Movie Agent")
-query = st.text_input("What do you want to know about movies?")
-if query:
-    response = generate_response(query)
-    st.write(response)
--- Neo4jTestAgent/genai-integration-langchain/genai-integration-langchain/neo4j_query.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
-
-# Create Neo4jGraph instance
-
-# Run a query and print the result
